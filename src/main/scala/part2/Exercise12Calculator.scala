55394ad45ea7f1aa72397fe8eb57e2c810c02c8c
Alok <alok.acharya@digital.hmrc.gov.uk>
2021-04-12 12:03:20 +0100

    WIP calculator

--- src/main/scala/part2/Exercise12Calculator.scala
+++ src/main/scala/part2/Exercise12Calculator.scala
@@ -1,11 +1,19 @@
 package part2
 
+import part2.Calculator.eval
+
 // ----------------------------------------------
 
 // Step 1. Write a definition for Expr here!
 
 sealed abstract class Expr
 
+final case class Num(int:Double) extends Expr
+final case class Addition(int1: Expr, int2: Expr) extends Expr
+final case class Substraction(int1: Expr, int2: Expr) extends Expr
+final case class Multiplication(int1: Expr, int2: Expr) extends Expr
+final case class Division(int1: Expr, int2: Expr) extends Expr
+final case class Squareroot(int1: Expr) extends Expr
 // Handle the following types of equation:
 // - addition
 // - subtraction
@@ -16,20 +24,47 @@ sealed abstract class Expr
 // Give it a `stringify` method
 // that renders the expression as a string.
 
+
 // ----------------------------------------------
 
 // Step 2. Implement eval
 // for each of the "calculator" objects below:
 
 object Calculator {
+  def stringify(calc: Expr): String = {
+    calc match {
+      case Num(expr1) => s"${expr1}"
+      case Addition(int1, int2) => s"${stringify(int1)} + ${stringify(int2)} = ${eval(calc)}"
+      case Substraction(int1, int2) => s"${stringify(int1)} - ${stringify(int2)} = ${eval(calc)}"
+      case Multiplication(int1, int2) => s"${stringify(int1)} * ${stringify(int2)} = ${eval(calc)}"
+      case Division(int1, int2) => s"${stringify(int1)} / ${stringify(int2)} = ${eval(calc)}"
+      case Squareroot(x) => s" Square Root of $x = ${Math.sqrt(eval(x))}"
+
+    }
+  }
+
   def eval(calc: Expr): Double = {
-    ???
+    calc match {
+      case Num(x) => x
+      case Addition(expr1, expr2) => eval(expr1) + eval(expr2)
+      case Substraction(expr1, expr2) => eval(expr1) - eval(expr2)
+      case Multiplication(expr1, expr2) => eval(expr1) * eval(expr2)
+      case Division(expr1, expr2)  => eval(expr1) / eval(expr2)
+      case Squareroot(expr1) => Math.sqrt(eval(expr1))
+    }
   }
 }
 
 object IntCalculator {
   def eval(calc: Expr): Int = {
-    ???
+    calc match {
+      case Num(x) => x.toInt
+      case Addition(expr1, expr2) => eval(expr1) + eval(expr2)
+      case Substraction(expr1, expr2) => eval(expr1) - eval(expr2)
+      case Multiplication(expr1, expr2) => eval(expr1) * eval(expr2)
+      case Division(expr1,expr2) => eval(expr1) / eval(expr2)
+      case Squareroot(expr1) =>Math.sqrt(eval(expr1)).round.toInt
+    }
   }
 }
 
@@ -51,17 +86,23 @@ object Expr {
 }
 
 object Exercise11Calculator {
-  // val calc1 = Add(Num(1.1), Mul(Num(2.2), Num(3.3)))
-  // val calc2 = Add(Mul(Num(1.1), Num(2.2)), Num(3.3))
+  val calc1: Expr = Addition(Num(1.1), Multiplication(Num(2.2), Num(3.3)))
+  val calc2: Expr = Addition(Multiplication(Num(1.1), Num(2.2)), Num(3.3))
+
+
 
   def main(args: Array[String]): Unit = {
     println("stringify")
-    // println(calc1.stringify)
-    // println(calc2.stringify)
+
+     println(Calculator.stringify(calc1))
+     println(Calculator.stringify(calc2))
+
+    println()
 
     println("Calculator.eval")
-    // println(Calculator.eval(calc1))
-    // println(Calculator.eval(calc2))
+     println(Calculator.eval(calc1))
+     println(Calculator.eval(calc2))
+    println()
 
     println("IntCalculator.eval")
     // println(IntCalculator.eval(calc1))
--- src/main/scala/part2/Exercise13SafeCalculator.scala
+++ src/main/scala/part2/Exercise13SafeCalculator.scala
@@ -1,10 +1,17 @@
 package part2
 
+import part2.SafeCalculator.eval
+
 // ----------------------------------------------
 
 // Step 1. Implement a `Result` type
 
 sealed abstract class Result
+
+final case class Success(a: Double) extends Result
+
+final case class Fail(message: String = "Math Error") extends Result
+
 
 // A Result is:
 //   - a Pass containing a Double result, or
@@ -16,8 +23,55 @@ sealed abstract class Result
 
 object SafeCalculator {
   def eval(calc: Expr): Result = {
-    ???
+
+    calc match {
+      case Num(x) => Success(x)
+      case Addition(expr1, expr2) => eval(expr1) match {
+        case Success(x) => eval(expr2) match {
+          case Success(y) => Success(x + y)
+          case Fail(message) => Fail(message)
+        }
+        case Fail(message) => Fail(message)
+      }
+
+      case Substraction(expr1, expr2) => eval(expr1) match {
+        case Success(x) => eval(expr2) match {
+          case Success(y) => Success(x - y)
+          case Fail(message) => Fail(message)
+        }
+        case Fail(message) => Fail(message)
+      }
+
+      case Multiplication(expr1, expr2) => eval(expr1) match {
+        case Success(x) => eval(expr2) match {
+          case Success(y) => Success(x * y)
+          case Fail(message) => Fail(message)
+        }
+        case Fail(message) => Fail(message)
+      }
+      case Division(expr1, expr2) => eval(expr1) match {
+        case Success(x) => if (x == 0) {
+          Success(0)
+        }
+        else {
+          eval(expr2) match {
+            case Success(y) => if (y == 0) {
+              Fail("cannot divide by 0")
+            }
+            else {
+              Success(x / y)
+            }
+          }
+
+        }
+      }
+      case Squareroot(expr1) => eval(expr1) match {
+        case Success(x) => Success(Math.sqrt(x))
+        case Fail(message) => Fail(message)
+      }
+    }
   }
+
 }
 
 object Exercise13SafeCalculator {
